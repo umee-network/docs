7b4e33db243e378776c9d355093c8cbb9d2a70ab
elite0226 <elite0226@outlook.com>
2022-06-21 09:56:22 -0400

    add hover effect

--- docs/.vuepress/components/GradientBox.vue
+++ docs/.vuepress/components/GradientBox.vue
@@ -4,23 +4,30 @@
   </div>
 </template>
 
-<style lang="css">
+<style lang="scss">
 .GradientBox {
   position: relative;
   z-index: 1;
   border: none !important;
   padding: 18px 18px 30px;
   width: 100%;
+  border-radius: 5px;
+  transition-duration: 0.3s;
+
+  &:hover {
+    box-shadow: var(--box-shadow);
+    transform: translateY(-2px);
+  }
 }
 
 .GradientBox::before {
-  content: '';
+  content: "";
   position: absolute;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-  background: linear-gradient(180deg, #DDB4FF 0%, #4EFFE5 100%);;
+  background: linear-gradient(180deg, #ddb4ff 0%, #4effe5 100%);
   padding: 1px;
   border-radius: 5px;
   -webkit-mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
