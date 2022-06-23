1b536665d2841e3e709a643862b8cefb2c4e4d1d
elite0226 <elite0226@outlook.com>
2022-06-23 11:27:56 -0400

    update mobile navbar #4

--- docs/.vuepress/theme/components/ToggleColorModeButton.vue
+++ docs/.vuepress/theme/components/ToggleColorModeButton.vue
@@ -1,70 +1,31 @@
-<template>
-  <div class="container">
-    <a href="https://app.umee.cc/" target="_blank"
-      ><Button>Launch App</Button></a
-    >
-    <div class="dark-mode-widget" @click="toggleDarkTheme">
-      <img v-if="darkMode" src="/img/dark-mode.svg" class="theme-toggle" />
-      <img v-if="!darkMode" src="/img/light-mode.svg" class="theme-toggle" />
-    </div>
-  </div>
-</template>
+<script setup lang="ts">
+import {
+  useDarkMode,
+  useThemeLocaleData,
+} from "@vuepress/theme-default/lib/client";
 
-<script>
-export default {
-  data() {
-    return {
-      darkMode: false,
-    };
-  },
-  mounted() {
-    this.checkUserPreference();
-  },
-  methods: {
-    toggleDarkTheme() {
-      const body = window.document.querySelector("html");
-      body.classList.toggle("dark");
-      //If dark mode is selected
-      if (body.classList.contains("dark")) {
-        //Save user preference in storage
-        localStorage.setItem("dark-theme", "true");
-        this.darkMode = true;
-        //If light mode is selected
-      } else {
-        body.classList.remove("dark");
-        this.darkMode = false;
-        setTimeout(function () {
-          localStorage.removeItem("dark-theme");
-        }, 100);
-      }
-    },
-    checkUserPreference() {
-      //Check Storage on Page load. Keep user preference through sessions
-      if (localStorage.getItem("dark-theme")) {
-        const body = window.document.querySelector("html");
-        body.classList.add("dark");
-        this.darkMode = true;
-      } else this.darkMode = false;
-    },
-  },
+const themeLocale = useThemeLocaleData();
+const isDarkMode = useDarkMode();
+
+const toggleColorMode = (): void => {
+  isDarkMode.value = !isDarkMode.value;
 };
 </script>
 
-<style>
-.container {
-  display: flex;
-  align-items: center;
-  margin-left: 25px;
-}
+<template>
+  <button
+    class="toggle-color-mode-button"
+    :title="themeLocale.toggleColorMode"
+    @click="toggleColorMode"
+  >
+    <img v-if="isDarkMode" src="/img/dark-mode.svg" class="theme-toggle" />
+    <img v-if="!isDarkMode" src="/img/light-mode.svg" class="theme-toggle" />
+  </button>
+</template>
 
-.dark-mode-widget {
-  display: flex;
-  align-items: center;
-  margin-left: 15px;
-  cursor: pointer;
-}
+<style scoped>
 .theme-toggle {
   width: 22px;
-  height: 22px;
+  height: 23px;
 }
 </style>
---
+++ docs/.vuepress/theme/components/ToggleSidebarButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useThemeLocaleData } from '@vuepress/theme-default/lib/client'
+
+defineEmits(['toggle'])
+
+const themeLocale = useThemeLocaleData()
+</script>
+
+<template>
+  <div
+    class="toggle-sidebar-button"
+    :title="themeLocale.toggleSidebar"
+    aria-expanded="false"
+    role="button"
+    tabindex="0"
+    @click="$emit('toggle')"
+  >
+    <div class="icon" aria-hidden="true">
+      <img src="/img/Hamburger.svg" />
+    </div>
+  </div>
+</template>
