e319ce468caa9e6d42c3bf7a256719764da5763e
Brandon Comer <90658712+brandoncomer@users.noreply.github.com>
2022-06-25 14:52:36 -0700

    added users and validators menus

---
+++ docs/.vuepress/components/Developers.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>Validators</h1>
+    <p>Validators are nodes that participate in the security of a network and are responsible for committing new blocks in the blockchain. To learn more, select one of the options below.</p>
+    <div class="row two-columns">
+      <menu-card
+        v-for="(menu, index) in menus"
+        :key="index"
+        :img="menu.img"
+        :title="menu.title"
+        :text="menu.text"
+        :link="menu.link"
+        :flex="true"
+      />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import MenuCard from "./MenuCard.vue";
+export default {
+  components: { MenuCard },
+  data() {
+    return {
+      menus: [
+        {
+          img: "/img/docs-validator-icon.png",
+          title: "Testnet Validator",
+          text: "Turn a current node into a validator on the testnet",
+          link: "/validators/testnet-validator",
+        },
+        {
+          img: "/img/docs-validator-icon.png",
+          title: "Mainnet Validator",
+          text: "Turn a current node into a validator on the mainnet",
+          link: "/validators/mainnet-validator",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="css">
+.card {
+  display: flex;
+}
+</style>
---
+++ docs/.vuepress/components/Users.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h1>Users</h1>
+    <p>Supply and borrow funds for ultimate DeFi composability using Umee money markets.</p>
+    <div class="row two-columns">
+      <menu-card
+        v-for="(menu, index) in menus"
+        :key="index"
+        :img="menu.img"
+        :title="menu.title"
+        :text="menu.text"
+        :link="menu.link"
+        :flex="true"
+      />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import MenuCard from "./MenuCard.vue";
+export default {
+  components: { MenuCard },
+  data() {
+    return {
+      menus: [
+        {
+          img: "/img/docs-users-icon.png",
+          title: "Getting started",
+          text: "Learn the basics on how to use Umee",
+          link: "/users",
+        },
+        {
+          img: "/img/docs-users-icon.png",
+          title: "Staking Umee",
+          text: "Stake Umee to help secure the network and earn rewards",
+          link: "/users",
+        },
+        {
+          img: "/img/docs-users-icon.png",
+          title: "Using the Web App",
+          text: "Use the web app for easy access to DeFi",
+          link: "/users",
+        },
+        {
+          img: "/img/docs-users-icon.png",
+          title: "Participating in Governance",
+          text: "Use your vote to help shape Umee's future",
+          link: "/users",
+        },
+        {
+          img: "/img/docs-users-icon.png",
+          title: "Using testnet",
+          text: "Practice DeFi with test tokens",
+          link: "/users",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="css">
+.card {
+  display: flex;
+}
+</style>
